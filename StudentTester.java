b65bc641139da6fbd5317ee489104d749daa0675
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-06-19 20:55:36 +0530

    Update StudentTester.java

--- StudentTester.java
+++ StudentTester.java
@@ -7,23 +7,23 @@ public class StudentTester {
 	public static void main(String[] args) {
 	Scanner sc= new Scanner(System.in);
 	System.out.println("Enter the size of array ");
-	int n=sc.nextInt();
 	
-	Student[] arr=new Student[n];
+	Student[] arr=new Student[sc.nextInt()];
 	int i;
 	
-	for(i=0;i<arr.length;i++) {
+	for(i=0;i<arr.length;i++) 
+	{
 		System.out.println("Enter the Student Details");
 		System.out.println("Name,Address,Branch,Marks");
-	Student s=new Student(sc.next(),sc.next(),sc.next(),sc.nextDouble());
-arr[i]=s;
+		Student s=new Student(sc.next(),sc.next(),sc.next(),sc.nextDouble());
+		arr[i]=s;
 	
 	}
 	System.out.println("Student Details...");
 
 	for(i=0;i<arr.length;i++) {
 		
-System.out.println(arr[i].toString());
+	System.out.println(arr[i].toString());
 	}
 	
 	
